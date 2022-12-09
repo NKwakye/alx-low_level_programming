e825679d2ff56f6de76abd9f3813e5190daaedfe
NKwakye <dkwakye94@gmail.com>
2022-12-09 10:21:28 -0800

    Task 10: Print All possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible different combinations of two digitss
+ *
+ * Return: Always 0 (Success)
+ * 
+ */
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+
+	for (tens = '0'; tens <= '9'; ones++)/* Prints tens */
+	{
+		for (ones = '0'; ones <= '9'; ones++)/* Prints ones */
+		{
+			if (!((ones == tens) || (tens > ones)))/*prevents repetition */
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8'))/* adds comma and space*/
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -8,3 +8,6 @@ Task 6. Numberz
 Task 7. Smile in the mirror
 Task 8. Hexadecimal
 Task 9. Patience, persistence and perspiration make an unbeatable combination for success
+Task 10. Inventing is a combination of brains and materials. The more brains you use, the less material you need
+Task 11. The success combination in business is: Do what you do better... and: do more of what you do...
+Task 12. Software is eating the World
