00c72d76d02607853e007812feaa796ae5615ecc
NKwakye <dkwakye94@gmail.com>
2022-12-09 08:59:39 -0800

    Task 6: Print Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  main - Prints all single digit numbers of base 10 starting from 0 only using putchar and without char variables
+ *
+ *  Return: Always 0
+ */
+int main(void)
+{
+	int sdn;
+
+	for (sdn = 0; sdn < 10; sdn++)
+		putchar((sdn % 10) + '0');
+
+	putchar('\n');
+	return (0);
+}
