9603e1fe721077bd7de6f5cf307d8c9369fc2c08
NKwakye <dkwakye94@gmail.com>
2022-12-09 10:26:19 -0800

    Task 10: Print All possible different combi
    nations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,14 +4,14 @@
  * main - Prints all possible different combinations of two digitss
  *
  * Return: Always 0 (Success)
- * 
  */
 int main(void)
+
 {
 	int ones = '0';
 	int tens = '0';
 
-	for (tens = '0'; tens <= '9'; ones++)/* Prints tens */
+	for (tens = '0'; tens <= '9'; tens++)/* Prints tens */
 	{
 		for (ones = '0'; ones <= '9'; ones++)/* Prints ones */
 		{
