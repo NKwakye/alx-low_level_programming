d2524315060fb19dc5daee7ce9c5f87e31fe8b44
NKwakye <dkwakye94@gmail.com>
2022-12-09 08:27:42 -0800

    Task 3: Print Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  main - returns alphabets both in lower and upper case
+ *
+ *   Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int alp = 'a';
+	int ALP = 'A';
+
+	while (alp <= 'z')
+	{
+		putchar(alp);
+		alp++;
+	}
+	while (ALP <= 'Z');
+	{
+		putchar(ALP);
+		ALP++;
+	}
+
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 Task 0: 0-positive_or_negative.c
 Task 1: 1-last_digit.c
 Task 2: 2-print_alphabet.c
+Task 3: 3-print_alphabets.c
