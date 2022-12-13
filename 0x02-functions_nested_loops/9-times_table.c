3f617e85f4f23c36a185e1679f05a39face1ac56
NKwakye <dkwakye94@gmail.com>
2022-12-13 06:37:52 -0800

    Task 9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 			c = a * b;
 			if ((c / 10) == 0)
 			{
-				if (b ==)
+				if (b == 0)
 				{
 					_putchar('0');
 				}
@@ -41,6 +41,6 @@ void times_table(void)
 			}
 		}
 	}
-	_putchar('\n')
+	_putchar('\n');
 	}
 }
