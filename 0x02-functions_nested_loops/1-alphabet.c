ef13e5babc54a6cd7dea07705953fc1c01c197f2
NKwakye <dkwakye94@gmail.com>
2022-12-13 04:18:19 -0800

    Task 1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet - Function that prints the alphabet, in lowercase, followed by a new line.
+ * print_alphabet - Function that prints the alphabet, in lowercase,
+ * followed by a new line.
  *
  * Return 0 (Success)
  */
