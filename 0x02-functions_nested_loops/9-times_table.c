a92238d24479d4c242b06c19b7264cac67d949f7
NKwakye <dkwakye94@gmail.com>
2022-12-13 06:40:51 -0800

    Task 9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,9 +8,9 @@ void times_table(void)
 {
 	int a, b, c;
 
-	for (a = 0; a < 9; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b < 9; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
 			if ((c / 10) == 0)
