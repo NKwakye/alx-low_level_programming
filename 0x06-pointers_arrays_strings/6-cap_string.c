52f96f11a03416b4d1d846a607b1ee6e1179a08c
NKwakye <56074884+NKwakye@users.noreply.github.com>
2022-12-22 00:44:19 +0000

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,47 @@
-#include "main.h"
+#include "holberton.h"
+
+#define SEPARATORS " \t\n,;.!?\"(){}"
 
 /**
- * cap_string - function that capitalizes all words of a string.
- * @str: The string to be capitalized.
+ * member - tests if a string contains a character
+ * @c: the character
+ * @s: the string
  *
- * Return: A pointer to the changed string
+ * Return: 1 if found, 0 otherwise
  */
-
-char *cap_string(char *str)
+int member(char c, char *s)
 {
-	int i;
-	int j;
-	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
+	for (; *s; s++)
+		if (*s == c)
+			return (1);
+	return (0);
+}
 
-	i = 0;
+/**
+ * _islower - checks if c is lowercase
+ * @c: the character to test case
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
 
-	while (str[i] != '\0')
-	{
-		if (i == 0 && str[i] >= 97 && str[i] <= 122)
-		{
-			str[i] = str[i] - 32;
-		}
-		j = 0;
-		while (c[j] != '\0')
-		{
-			if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
-			{
-				str[i + 1] = str[i + j] - 32;
-			}
-			j++;
-		}
-		i++;
-	}
-	return (str);
+/**
+ * cap_string - capitalizes each word of a string
+ * @s: the string to capitalize
+ *
+ * Return: char pointer
+ */
+char *cap_string(char *s)
+{
+	char *ret = s;
+
+	if (_islower(*s))
+		*s += 'A' - 'a';
+	while (*++s)
+		if (_islower(*s) && member(*(s - 1), SEPARATORS))
+			*s += 'A' - 'a';
+	return (ret);
 }
