b3f6173f97f16d77786710bbf87cd9e106a8867b
NKwakye <dkwakye94@gmail.com>
2023-01-16 04:07:28 -0800

    Task 0 to 4

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.H>
+
+/**
+ * main - Prints the name of the file program was compiled from
+ * Return - Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
