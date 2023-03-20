5d5aa8e6c7b5877a1624d4207579432b3e85ecfd
Nana Kwakye Owusu <dkwakye94@gmail.com>
2023-03-20 05:10:53 -0700

    TASK 1

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget _P /tmp -q "https://github.com/NKwakye/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmmw.so"
-export LD_PRELOAD="/tmp/libmmw.so"
+wget _P /tmp -q "https://github.com/NKwakye/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so"
+export LD_PRELOAD="/tmp/iwin.so"
