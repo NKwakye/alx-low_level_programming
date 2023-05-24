c97e95efcc3e245b30ffd7c83bcdb1c6daf1b94c
Nana Kwakye Owusu <dkwakye94@gmail.com>
2023-05-24 05:11:40 -0700

    Update

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,20 +1,41 @@
 #!/usr/bin/python3
-""" Island Perimeter """
+"""
+    This is the island_perimeter module.
+"""
+
+
+def neighbour(grid, y, x):
+    """
+        Finds the number of neighbours of a square.
+
+        Args:
+            grid (list): the matrix.
+            y (int): height parameter.
+            x (int): width parameter.
+    """
+    nb = 4
+
+    if (y > 0) and grid[y - 1][x]:
+        nb -= 1
+    if (x > 0) and grid[y][x - 1]:
+        nb -= 1
+    if (y < len(grid) - 1) and grid[y + 1][x]:
+        nb -= 1
+    if (x < len(grid[0]) - 1) and grid[y][x + 1]:
+        nb -= 1
+    return nb
 
 
 def island_perimeter(grid):
-    """ Island Perimeter """
-    peri = 0
-    gl = []
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            gl.append(grid[i][j])
-    for i in range(len(gl)):
-        if i < len(gl) - 1 and i > 0:
-            if gl[i] == 0 and gl[i + 1] == 1 or gl[i] == 0 and gl[i - 1] == 1:
-                peri += 1
-    if peri % 2 == 0:
-        peri *= 2
-    else:
-        peri = (peri * 2) - 1
-    return peri
+    """
+        Finds the island perimeter.
+
+        Args:
+            grid (list): the matrix.
+    """
+    perimeter = 0
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            if grid[y][x] == 1:
+                perimeter += neighbour(grid, y, x)
+    return perimeter
